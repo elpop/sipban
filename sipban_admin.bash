fd7f795895b9582bb9e67d65f1a727e6f71e9bf6
Fernando Romo <pop@cofradia.org>
2024-12-04 13:32:59 -0600

    regex to validate ip/class and hash:net on create to support ip/class blocks

--- sipban_admin.bash
+++ sipban_admin.bash
@@ -7,11 +7,13 @@
 # Your ipset data set name
 IPSET_NAME="sipban"
 SAVE_FILE="/etc/sipban.dump"
+TIMEOUT=604800
+IPv4="^((25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})[.](25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})[.](25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})[.](25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))(\/(3[0-2]|[1-2][0-9]|[1-9]))?$"
 
 case $1 in
     # create set
     -c)
-        /usr/sbin/ipset create $IPSET_NAME hash:ip hashsize 4096 timeout 604800 > /dev/null
+        /usr/sbin/ipset create $IPSET_NAME hash:net hashsize 4096 timeout $TIMEOUT > /dev/null
     ;;
     # Flush (empty) the set
     -f)
@@ -27,7 +29,7 @@ case $1 in
     ;;
     # add ip to set
     -a)
-        if [[ $2  =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        if [[ $2  =~ $IPv4 ]]; then
             /usr/sbin/ipset -q add $IPSET_NAME $2 > /dev/null
         else
             echo 'Invalid ip address'
@@ -35,7 +37,7 @@ case $1 in
     ;;
     # Delete ip from set
     -d)
-        if [[ $2  =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        if [[ $2  =~ $IPv4 ]]; then
             /usr/sbin/ipset del $IPSET_NAME $2
         else
             echo 'Invalid ip address'
@@ -43,7 +45,7 @@ case $1 in
     ;;
     # list ip's on set
     -l)
-        if  [[ $2  =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        if  [[ $2  =~ $IPv4 ]]; then
             /usr/sbin/ipset test $IPSET_NAME $2
         else
             /usr/sbin/ipset list $IPSET_NAME
@@ -62,7 +64,7 @@ case $1 in
         if test -f $2; then
             while IFS= read -r line;
                 do
-                    if [[ $line  =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+                    if [[ $line  =~ $IPv4 ]]; then
                         /usr/sbin/ipset -q add $IPSET_NAME $line > /dev/null
                     else
                         echo 'Invalid ip address'
@@ -79,7 +81,7 @@ case $1 in
         echo '    -k (destroy sipban set)'
         echo '    -i (info of sipban set)'
         echo
-        echo '    -a [ip] (add ip to sipban set)'
+        echo '    -a [ip] or [ip/class] (add ip or ip/class to sipban set)'
         echo '    -d [ip] (delete ip from sipban set)'
         echo '    -l {ip} (list members or test a given ip)'
         echo
